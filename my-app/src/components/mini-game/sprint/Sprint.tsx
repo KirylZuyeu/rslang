9846f34ef0ef368bc88a976cdf8328e609697573
katusha-2010 <katusha-2010@mail.ru>
2022-08-19 14:09:16 +0300

    fix: correct Sprint.tsx

--- my-app/src/components/mini-game/sprint/Sprint.tsx
+++ my-app/src/components/mini-game/sprint/Sprint.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+import GameSprint from './GameSprint';
+import ListLevels from './ListLevels';
 import styles from './sprint.module.css';
+import base from './base.json';
 
 function Sprint() {
+	const [start, setCount] =useState(true);
 	
 	return (
-		<div>Sprint Game</div>
+		<div>
+			{
+				start
+				? <div className={styles.sprint}>
+					<div className={styles.info}>
+						<p>Спринт - это игра для повторения изученных слов</p>
+						<div>Выбери уровень сложности
+							<br></br>
+						    <ListLevels/>
+						</div>
+						<div>
+							<button className={styles.btnStart} onClick={()=>setCount(!start)}>Начать</button>
+						</div>
+					</div>
+				</div> 
+				: <GameSprint base={base}/>
+			}
+		</div>		
 	)
 }
 

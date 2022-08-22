a16189802a7761d162727a6ca55661778399382d
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-08-22 14:07:53 +0300

    fix: api and registration

--- my-app/src/functionality/api.tsx
+++ my-app/src/functionality/api.tsx
@@ -1,4 +1,5 @@
-const url = 'https://react-learnwords-example.herokuapp.com';
+const url = 'https://rslang-be-server.herokuapp.com';
+
 const words = `${url}/words`;
 const users = `${url}/users`;
 const signin = `${url}/signin`;
@@ -15,11 +16,11 @@ type ChengeDataUser = {
 }
 
 //=================WORDS=================
-export const getWords = async (groupNumber: number, page: number) => await fetch(`${words}?group=${groupNumber}&page=${page}`);
-export const getWord = async (id: number) => await fetch(`${words}/${id}`);
+export const getWords = async (groupNumber: number, page: number) => (await fetch(`${words}?group=${groupNumber}&page=${page}`)).json();
+export const getWord = async (id: number) => (await fetch(`${words}/${id}`)).json();
 
 //==================USERS================
-export const greateCar = async (data: DataUser) => {
+export const createUser = async (data: DataUser) => 
 	(await fetch(users, {
 		method: 'POST',
 		body: JSON.stringify({
@@ -29,11 +30,11 @@ export const greateCar = async (data: DataUser) => {
 		}),
 		headers: { 'Content-Type': 'application/json' }
 	})).json()
-}
 
-export const getUser = async (id: number) => await fetch(`${users}/${id}`);
 
-export const changeCar = async (id: number, data: ChengeDataUser) => {
+export const getUser = async (id: number) => (await fetch(`${users}/${id}`)).json();
+
+export const changeUser = async (id: number, data: ChengeDataUser) => 
 	(await fetch(`${users}/${id}`, {
 		method: 'PUT',
 		body: JSON.stringify({
@@ -42,17 +43,17 @@ export const changeCar = async (id: number, data: ChengeDataUser) => {
 		}),
 		headers: { 'Content-Type': 'application/json' }
 	})).json()
-}
 
-export const delUser = async (id: number) => await (await (fetch(`${users}/${id}`, { method: 'DELETE' }))).json();
 
-export const getUserTokens = async (id: number) => await fetch(`${users}/${id}/tokens`);
+export const delUser = async (id: number) => await (fetch(`${users}/${id}`, { method: 'DELETE' }));
+
+export const getUserTokens = async (id: number) => (await fetch(`${users}/${id}/tokens`)).json();
 
 //=============USERS WORDS===============
 
-export const getUserWords = async (id: number) => await fetch(`${users}/${id}/words`);
+export const getUserWords = async (id: number) => (await fetch(`${users}/${id}/words`)).json();
 
-export const createUserWord = async (userId: number, wordId: number) => {
+export const createUserWord = async (userId: number, wordId: number) => 
 	(await fetch(`${users}/${userId}/words/${wordId}`, {
 		method: 'POST',
 		body: JSON.stringify({
@@ -63,11 +64,11 @@ export const createUserWord = async (userId: number, wordId: number) => {
 		}),
 		headers: { 'Content-Type': 'application/json' }
 	})).json()
-}
 
-export const getUserWord = async (userId: number, wordId: number) => await fetch(`${users}/${userId}/words/${wordId}`);
 
-export const changeUserWord = async (userId: number, wordId: number) => {
+export const getUserWord = async (userId: number, wordId: number) => (await fetch(`${users}/${userId}/words/${wordId}`)).json();
+
+export const changeUserWord = async (userId: number, wordId: number) => 
 	(await fetch(`${users}/${userId}/words/${wordId}`, {
 		method: 'PUT',
 		body: JSON.stringify({
@@ -78,24 +79,24 @@ export const changeUserWord = async (userId: number, wordId: number) => {
 		}),
 		headers: { 'Content-Type': 'application/json' }
 	})).json()
-}
+
 
 export const delUserWord = async (userId: number, wordId: number) =>
-	await (await (fetch(`${users}/${userId}/words/${wordId}`, { method: 'DELETE' }))).json();
+	await (fetch(`${users}/${userId}/words/${wordId}`, { method: 'DELETE' }));
 
 //============Users AggregatedWords========================
 
 export const getUsersAggregatedWords = async (id: number, page: number = 1, wordsPerPage: number = 1, group: string = '') =>
-	await fetch(`${users}/${id}/aggregatedWords?page=${page}&wordsPerPage=${wordsPerPage}&group=${group}`);
+	(await fetch(`${users}/${id}/aggregatedWords?page=${page}&wordsPerPage=${wordsPerPage}&group=${group}`)).json();
 
 export const getUsersAggregatedWord = async (userId: number, wordId: number) =>
-	await fetch(`${users}/${userId}/aggregatedWords/${wordId}`);
+	(await fetch(`${users}/${userId}/aggregatedWords/${wordId}`)).json();
 
 //================Users Statistic=======================
 
-export const getUserStatistic = async (id: number) => await fetch(`${users}/${id}/statistics`);
+export const getUserStatistic = async (id: number) => (await fetch(`${users}/${id}/statistics`)).json();
 
-export const changeUserStatistic = async (id: number) => {
+export const changeUserStatistic = async (id: number) => 
 	(await fetch(`${users}/${id}/statistics`, {
 		method: 'PUT',
 		body: JSON.stringify({
@@ -108,12 +109,12 @@ export const changeUserStatistic = async (id: number) => {
 		}),
 		headers: { 'Content-Type': 'application/json' }
 	})).json()
-}
+
 //================Users Setting=======================
 
-export const getUserSetting = async (id: number) => await fetch(`${users}/${id}/setting`);
+export const getUserSetting = async (id: number) => (await fetch(`${users}/${id}/setting`)).json();
 
-export const changeUserSetting = async (id: number) => {
+export const changeUserSetting = async (id: number) => 
 	(await fetch(`${users}/${id}/setting`, {
 		method: 'PUT',
 		body: JSON.stringify({
@@ -126,19 +127,18 @@ export const changeUserSetting = async (id: number) => {
 		}),
 		headers: { 'Content-Type': 'application/json' }
 	})).json()
-}
+
 
 //=================Sign In====================
 
-export const signIn = async () => {
+export const signIn = async (data: ChengeDataUser) =>
 	(await fetch(signin, {
 		method: 'POST',
-		body: JSON.stringify({
-			//-----------------------------------
-			"email": "string",
-			"password": "string"
-			//-------------------------------------------
+		body: JSON.stringify({			
+			email: data.email,
+			password: data.password			
 		}),
 		headers: { 'Content-Type': 'application/json' }
 	})).json()
-}
+
+
